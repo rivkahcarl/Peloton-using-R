9c9c8bd1e601080957fd51939770c4e53c030d86
AnfieldWest <41492219+AnfieldWest@users.noreply.github.com>
2018-10-06 08:22:01 -0700

    Delete API Credentials.R

--- API Credentials.R
+++
@@ -1,6 +0,0 @@
-# Peloton API Credentials
-username <- "your.peloton.login"
-password <- "your.peloton.password"
-base.url  <- "https://api.pelotoncycle.com/api/"
-auth.url <- paste0("https://api.pelotoncycle.com/auth/login")
-userid <- "your.peloton.user.id"
