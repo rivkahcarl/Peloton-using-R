385fafac8562924512541783883089dd996beec6
AnfieldWest <41492219+AnfieldWest@users.noreply.github.com>
2018-10-06 08:24:07 -0700

    Create API Credentials.R

---
+++ scripts/API Credentials.R
@@ -0,0 +1,6 @@
+# Peloton API Credentials
+username <- "your.peloton.login"
+password <- "your.peloton.password"
+base.url  <- "https://api.pelotoncycle.com/api/"
+auth.url <- paste0("https://api.pelotoncycle.com/auth/login")
+userid <- "your.peloton.user.id"
